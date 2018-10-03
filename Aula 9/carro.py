ec86e1848a4e185b17ecef4ecd8d0a1ad2d709f3
gabrielmmelo <gabrielmelocomp@gmail.com>
2018-10-03 09:58:01 -0300

    Atualizando o carro

--- Aula 9/carro.py
+++ Aula 9/carro.py
@@ -1,11 +1,15 @@
 class Carro():
-    def __init__(self, cor, ano, numero_portas):
+    def __init__(self, rodas, janela, porta_mala, tipo_combustivel, cor, ano, numero_portas):
        self.cor = cor
        self.ano = ano
        self.numero_portas = numero_portas
+       self.rodas = rodas
+       self.janela = janela
+       self.porta_mala = porta_mala
+       self.tipo_combustivel = tipo_combustivel
 
     def exibir_caracteristicas(self):
         print ("Cor: " + self.cor + " Ano: " + str(self.ano) + " Número de portas: " + str(self.numero_portas))
 
-fusca = Carro("azul", 1942, 2)
+fusca = Carro("azul", 2)
 fusca.exibir_caracteristicas()
